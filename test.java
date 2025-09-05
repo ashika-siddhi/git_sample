000f78cdc706ad03b1c586db89d2b25d7f8c3170
Ashika <ashikasiddhi@gmail.com>
2025-09-05 14:32:49 +0530

    Updated file1.txt and test.java with latest changes

--- test.java
+++ test.java
@@ -6,10 +6,15 @@ public class test{
 
         int n= sc.nextInt();
         System.out.println(change(n));
+        System.out.println(minus(n));
         sc.close();
     }
 
     static int change(int n){
         return n+1;
+    }
+
+    static int minus(int n){
+        return n-1;
     }
 }
